56267a973c482b301c1277cc3f1f3d844d5f22eb
seooooooooook <80299241+seooooooooook@users.noreply.github.com>
2021-05-25 12:44:39 +0900

    react3000

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Welcome(props){
+  return <h1>Hello, {props.name}</h1>
+}
+
+function App(){
+  return (
+    <div>
+      <Welcome name="Kim" />
+      <Welcome name="jin" />
+      <Welcome name="seok" />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
