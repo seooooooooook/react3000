f055903ea7ccdb65d6dc28b2a5bd3d319033a68a
seooooooooook <80299241+seooooooooook@users.noreply.github.com>
2021-05-25 14:13:29 +0900

    2021-05-25

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props){
-  return <h1>Hello, {props.name}</h1>
-}
-
-function App(){
+function Comment(props){
   return (
-    <div>
-      <Welcome name="Kim" />
-      <Welcome name="jin" />
-      <Welcome name="seok" />
+    <div className="comment">
+      <div className="user-info">
+        <img className="avatar" src={props.author.avatarUrl} alt={props.author.name} />
+        <div className="user-name">{props.author.name}</div>
+      </div>
+      <div className="comment-text">{props.Text}</div>
+      <div className="comment-date">{formatDate(props.date)}</div>
     </div>
   )
 }
 
-ReactDOM.render(<App />,document.getElementById('root'));
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+const comment ={
+  date: new Date(),
+  Text: "열심히 할수록 기회는 따른다.",
+  author: {
+    name: "wlstjr519",
+    avatarUrl:
+      "http://wlstjr519.dothome.co.kr/portfolio/assets/img/aboutImg02.jpg"
+  }
+}
+
+ReactDOM.render(<Comment date={comment.date} text={comment.Text} author={comment.author}/>,document.getElementById('root'));
 
